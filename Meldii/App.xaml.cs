349f89ea987b051317a637d1f885038526d4de40
Goomii <kneehighpeas@gmail.com>
2015-01-30 22:03:23 +0000

    More error logging

--- Meldii/App.xaml.cs
+++ Meldii/App.xaml.cs
@@ -6,14 +6,29 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
-namespace MeldiiWpf
+namespace Meldii
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.StackTrace, e.Exception.Message);
+            string[] lines = 
+            {
+                "Source: " + e.Exception.Source,
+                "Target: " + e.Exception.TargetSite,
+                "Message: " + e.Exception.Message,
+                "\n",
+                e.Exception.StackTrace,
+                "\n"
+            };
 
+            System.IO.File.WriteAllLines(@"Meldii Crash more tears.txt", lines);
+        }
     }
 }
